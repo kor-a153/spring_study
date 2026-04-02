bb818dc575dcc338163e6e8b8dd29930ea3c7af0
kor-a153 <yuo0205@gmail.com>
2026-04-02 14:26:49 +0900

    0402spring

--- mybatis/src/test/java/com/app/mybatis/mapper/MapperTests.java
+++ mybatis/src/test/java/com/app/mybatis/mapper/MapperTests.java
@@ -1,6 +1,9 @@
 package com.app.mybatis.mapper;
 
+import com.app.mybatis.domain.vo.MemberVO;
+import com.app.mybatis.domain.vo.PostVO;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,16 +19,64 @@ public class MapperTests {
     private MemberMapper memberMapper;
 
     @Test
-    public void MapperTests() {
+    public void mapperTests() {
         log.info(timeMapper.getTime());
     }
 
     @Test
-    public void MapperTests2(){
+    public void mapperTests2() {
         log.info(timeMapper.getTime2());
     }
+
     @Test
-    public void MapperTests3() {
-        log.info("{}",memberMapper.selectAll());
+    public void mapperTests3() {
+        log.info("{}", memberMapper.selectAll());
+    }
+
+    @Test
+    public void mapperTests4() {
+        memberMapper.select(1L).map(MemberVO::toString).ifPresent(log::info);
+    }
+
+    //    @Test
+//    public void insertTest() {
+//        MemberVO memberVO = new MemberVO();
+//        memberVO.setMemberEmail("test456@gmail.com");
+//        memberVO.setMemberPassword("test123!@#");
+//        memberVO.setMemberName("장보고");
+//
+//        memberMapper.insert(memberVO);
+//
+//    }
+    @Test
+    public void updateTest() {
+        MemberVO memberVO = new MemberVO();
+        memberVO.setId(2L);
+        memberVO.setMemberEmail("test456@gmail.com");
+        memberVO.setMemberPassword("test123!@#");
+        memberVO.setMemberName("장길동");
+
+        memberMapper.select(2L).map(MemberVO::toString).ifPresent(log::info);
+        memberMapper.update(memberVO);
+        memberMapper.select(2L).map(MemberVO::toString).ifPresent(log::info);
+    }
+
+    @Test
+    public void deleteTest() {
+        memberMapper.delete(2L);
+    }
+
+    @Test
+    public void insertTest() {
+        MemberVO memberVO = new MemberVO();
+//        memberVO.setMemberEmail("test789@gmail.com");
+//        memberVO.setMemberPassword("test123!@#");
+//        memberVO.setMemberName("장보고");
+
+        memberVO.setMemberEmail("test456@gmail.com");
+        memberVO.setMemberPassword("test123!@#");
+        memberVO.setMemberName("이순신");
+
+        memberMapper.insert(memberVO);
     }
 }
