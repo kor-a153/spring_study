94affc3abf60267351e7cf291398196c3aea7f1c
kor-a153 <yuo0205@gmail.com>
2026-04-07 17:46:25 +0900

    0407Spring

--- controller/src/main/java/com/app/controller/controller/ExampleController.java
+++ controller/src/main/java/com/app/controller/controller/ExampleController.java
@@ -1,14 +1,16 @@
 package com.app.controller.controller;
 
+import com.app.controller.domain.vo.MemberVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.management.Attribute;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 @Slf4j
 @Controller
@@ -18,51 +20,79 @@ public class ExampleController {
     @GetMapping("ex01")
     public String ex01(String name, int age){
         log.info("ex01 응답 완료");
-        log.info("name : {}, age : {} , realAge: {}" ,name,age,age-1);
+        log.info("name : {}, age : {}, realAge: {}", name, age, age - 1);
 
-//        get방식으로 데이터를 어떻게 보내야 하는가?
-//        이름과 나이를 요청보내고,
-//        이름과 나이, 만나이를 로그에 출력하기
+        // GET방식으로 데이터를 어떻게 보내야 하는가?
+        // 이름과 나이를 요청보내고,
+        // 이름과 나이, 만나이를 로그에 출력하기
 
-
-        /*ex01.html */
-//        ex01 -> templates/ex01.html
-//        ex/ex/ex01 -> templates/ex/ex/ex01.html
-//        응답되는 페이지의 파일 경로
-        return "ex01";
-    }
-    @PostMapping("/user/info")
-    public String info(String name, Integer age) {
-        int Age = age - 1;
-
-        System.out.println("이름: " + name);
-        System.out.println("나이: " + age);
-        System.out.println("만나이: " + Age);
-
+        // ex01.html
+        // ex01 -> templates/ex01.html
+        // ex/ex/ex01 -> templates/ex/ex/ex01.html
+        // 응답되는 페이지의 파일 경로
         return "ex01";
     }
 
-//    @GetMapping("ex03")
-//    public String ex03 (Model model) {
-//        ArrayList<String> namess = new
-//    }
+    @PostMapping("ex01")
+    public void ex01Post(String name, int age){
+//        log.info("ex01 응답!");
+        log.info("name : {}, age : {}, realAge: {}", name, age, age - 1);
+    }
 
-//    ModelAttribute : 반드시 쿼리스트링 값을 전달해야 한다.
+    @GetMapping("ex02")
+    public String ex02(String name, Model model){
+        model.addAttribute("name", name);
+        return "ex02";
+    }
+
+    @GetMapping("ex03")
+    public String ex03(Model model){
+        ArrayList<String> names = new ArrayList<>(Arrays.asList("홍길동", "장보고", "이순신"));
+        model.addAttribute("names", names);
+        return "ex03";
+    }
+
+
+//    ModelAttribute: 반드시 쿼리스트링 값을 전달해야한다.
     @GetMapping("ex04")
-    public String ex04(@ModelAttribute("name")String name){
+    public String ex04(@ModelAttribute("name") String name){
         return "ex04";
     }
 
 //    ex05
-//    Model 어노테이션 활용
+//    Model 어노테이션 활용!
 //    이름, 취미를 받고
-//    화면에이름 :000
-//    취미: 000 으로 출력하기
+//    화면에 이름: OOO
+//    취미: OOO으로 출력하기
     @GetMapping("ex05")
-    public String ex05(@ModelAttribute("name")String name,
-                       @ModelAttribute("hobby") String hobby){
+    public String ex05(
+        @ModelAttribute("name") String name,
+        @ModelAttribute("hobby") String hobby
+    ){
         return "ex05";
     }
 
+    @GetMapping("ex06")
+    public String goToEx06(){
+        return "ex06";
+    }
+
+    @GetMapping("ex06-complete")
+    public String ex06Complete(
+            @ModelAttribute("memberName") String memberName
+    ){
+        log.info("memberName : {}", memberName);
+        return "ex06-complete";
+    }
+
+//    회원가입 완료 후
+//    페이지에 OOO님 환영합니다 출력
+    @PostMapping("ex06")
+    public String ex06(MemberVO memberVO){
+        log.info("응답이 들어옴!");
+        log.info("memberVO : {}", memberVO);
+//        return "redirect:/ex/ex06-complete?memberName=" + memberVO.getMemberName();
+        return "redirect:/ex/ex06-complete";
+    }
 
 }
